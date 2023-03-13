d1750c008bb73c64350ed98b22a4d0593fe989c9
FZ2161 <filipzalewski44@gmail.com>
2023-03-13 11:24:53 +0100

    sad

---
+++ client/app.js
@@ -0,0 +1,17 @@
+const body = document.getElementById('body')
+const peselID = document.getElementById('pesel')
+
+function saveVote1(){
+    var pesel = pesel.value
+    var kandydat = kandydat1
+
+}
+function saveVote2(){
+    var pesel = pesel.value
+    var kandydat = kandydat2
+}
+function saveVote3(){
+    var pesel = pesel.value
+    var kandydat = kandydat3
+
+}
