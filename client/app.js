fd305afef1b6d06b3a1aaef372676757ef69dd13
FZ2161 <filipzalewski44@gmail.com>
2023-03-21 13:37:07 +0100

    +admin

--- client/app.js
+++ client/app.js
@@ -6,42 +6,45 @@ const glos =  document.getElementById('zaglosowano')
 
 const baseURL = 'http://localhost:3000'
 
+
+
 async function glosoj(){
 
     const nowyPesel = pesel.value
     const nowyKandydat = wybrany.value
 
     //potwierdzenie poprawności
-    if(nowyPesel!='' && nowyKandydat=='k1' || nowyKandydat=='k2' || nowyKandydat=='k3'){
+    if((nowyPesel != '') && (nowyKandydat == 'k1' || nowyKandydat == 'k2' || nowyKandydat == 'k3')){
         const zaglosowano = document.createElement('h2')
         zaglosowano.innerHTML = 'oddano głos'
         zaglosowano.setAttribute('id', 'glos')
 
         app.appendChild(zaglosowano)
 
-        setTimeout(() => {
+          setTimeout(() => {
             zaglosowano.remove();
           }, 4000);
+
+        var url = `${baseURL}/add/${pesel.value}/${wybrany.value}`
+
+        await fetch(url)
+
+        pesel.value = ''
+        wybrany.value = ''
+
+        
     }else{
 
-        const zaglosowano = document.createElement('h2')
-        zaglosowano.innerHTML = 'nie udało sie oddać głosu z powodu niepoprawnych danych'
-        zaglosowano.setAttribute('id', 'red')
+        const nieZaglosowano = document.createElement('h2')
+        nieZaglosowano.innerHTML = 'nie udało sie oddać głosu z powodu niepoprawnych danych'
+        nieZaglosowano.setAttribute('id', 'red')
 
-        app.appendChild(zaglosowano)
+        app.appendChild(nieZaglosowano)
 
             setTimeout(() => {
-                zaglosowano.remove();
+                nieZaglosowano.remove();
               }, 4000);
     }
-      
-    //wysyłanie
-
-    const url = `${baseURL}/add/${pesel.value}/${wybrany.value}`
-
-    await fetch(url)
-
-    pesel.value = ''
-    wybrany.value = ''
+         
 
 }
